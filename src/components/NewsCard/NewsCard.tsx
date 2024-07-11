8c5d814ae6543f338e356768a3ae1581bd4b32d2
Aitegin <jyldyzbekov06aiti@gmail.com>
2024-07-11 21:13:18 +0600

    feat:date

--- src/components/NewsCard/NewsCard.tsx
+++ src/components/NewsCard/NewsCard.tsx
@@ -1,3 +1,4 @@
+import { formatDate } from 'shared/assets/formatDate/formatDate';
 import scss from './NewsCard.module.scss';
 import React from 'react';
 
@@ -12,12 +13,12 @@ interface NewsCardProps {
 const NewsCard: React.FC<NewsCardProps> = React.memo(({ img, date, title, desc, path }) => {
   return (
     <div className={scss.newsCard}>
-      
+
       <div className={scss.c_img}>
-        <img src={img} alt=''/>
+        <img src={img} alt='' />
       </div>
       <div className={scss.c_bottom}>
-        <p className={scss.date}>{date}</p>
+        <p className={scss.date}>{formatDate(date)}</p>
         <p className={scss.title}>{title}</p>
         <p className={scss.desc}>{desc}</p>
         <a href={`/news/${path}`}>Читать дальше →</a>
