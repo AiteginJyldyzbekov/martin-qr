cf75a7ede6c01a04507bde26bddaa1702254acde
Aitegin <jyldyzbekov06aiti@gmail.com>
2024-07-12 22:06:10 +0600

    fix:mainBanner

--- src/components/MainBanner/MainBanner.tsx
+++ src/components/MainBanner/MainBanner.tsx
@@ -2,9 +2,21 @@ import React, { useState, useEffect } from 'react';
 import scss from './MainBanner.module.scss';
 
 const backgroundImages = [
-  { url: "https://media.licdn.com/dms/image/D4D12AQGPCed4zlkFCA/article-cover_image-shrink_720_1280/0/1678631732312?e=2147483647&v=beta&t=XHMwdbaB1omlDGgJI-QMewqeFmDmYpDEk4AcC1-mu8c" },
-  { url: "https://cache.cosmopolitan.fr/data/photo/w1000_ci/6d/fashion-week-infos.jpg" },
-  { url: "https://footwearnews.com/wp-content/uploads/2022/07/GettyImages-1407379141.jpg?w=700&h=437&crop=1" }
+  {
+    url: "https://media.licdn.com/dms/image/D4D12AQGPCed4zlkFCA/article-cover_image-shrink_720_1280/0/1678631732312?e=2147483647&v=beta&t=XHMwdbaB1omlDGgJI-QMewqeFmDmYpDEk4AcC1-mu8c",
+    title: "Title 1",
+    desc: "Desc"
+  },
+  {
+    url: "https://cache.cosmopolitan.fr/data/photo/w1000_ci/6d/fashion-week-infos.jpg",
+    title: "Title 2",
+    desc: "Desc2"
+  },
+  {
+    url: "https://footwearnews.com/wp-content/uploads/2022/07/GettyImages-1407379141.jpg?w=700&h=437&crop=1",
+    title: "Title 3",
+    desc: "Desc3"
+  }
 ];
 
 function MainBanner() {
@@ -18,11 +30,13 @@ function MainBanner() {
     return () => clearInterval(interval); // Очистка интервала при размонтировании компонента
   }, []);
 
-  const currentImage = backgroundImages[currentImageIndex];
-  console.log(currentImage?.url);
-  
+  const currentImage = backgroundImages[currentImageIndex]?.url;
+  const currentTitle = backgroundImages[currentImageIndex]?.title
+  const currentDesc = backgroundImages[currentImageIndex]?.desc
+  // console.log(currentImage);
+
   // Проверка на наличие текущего изображения и его url
-  if (!currentImage || !currentImage?.url) {
+  if (!currentImage || !currentImage) {
     console.error("Текущее изображение не определено или не содержит URL");
     return null; // Или отобразите резервный UI
   }
@@ -30,12 +44,12 @@ function MainBanner() {
   return (
     <div
       className={`${scss.banner_w} container animate__animated animate__backInDown`}
-      style={{ backgroundImage: `url(${currentImage?.url})` }}
+      style={{ backgroundImage: `url(${currentImage})` }}
     >
       <div className={scss.banner_block}>
         <div className={scss.banner_b_top}>
-          <h1>Производство женской одежды под вашим брендом</h1>
-          <p>Полный комплекс услуг для СТМ: разработка коллекции одежды, разработка лекал и пошив образцов, подбор ткани, брендирование, пошив партии, отгрузка транспортными компаниями.</p>
+          <h1>{currentTitle}</h1>
+          <p>{currentDesc}</p>
         </div>
         <div className={scss.banner_b_bottom}>
           <button>Смотреть</button>
